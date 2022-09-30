a516e98279c2dc9f6a78418f6bfcb625b3c49d0e
prudhvirajgit <prudhviraj.borra@bizcloudexperts.com>
2022-09-30 11:32:58 +0530

    commit

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,4 +1,4 @@
-import "./Form.css";
+import "./form.css";
 export default function Form() {
   return (
     <>
@@ -9,81 +9,98 @@ export default function Form() {
         <div className="form_inner3">
           <div className="form_inner31">
             <label>Salutation</label>
-            <div className="form_rectangle"></div>
+            <br />
+            <input type={"text"} className="form_rectangle" />
           </div>
           <div className="form_inner31">
             <label>First Name</label>
-            <div className="form_rectangle"></div>
+            <br />
+            <input type={"text"} className="form_rectangle" />
           </div>
           <div className="form_inner31">
             <label>Middle Name</label>
-            <div className="form_rectangle"></div>
+            <br />
+            <input type={"text"} className="form_rectangle" />
           </div>
         </div>
         <div className="form_inner3">
           <div className="form_inner31">
             <label>Last Name</label>
-            <div className="form_rectangle"></div>
+            <br />
+            <input type={"text"} className="form_rectangle" />
           </div>
           <div className="form_inner31">
             <label>Tittle</label>
-            <div className="form_rectangle"></div>
+            <br />
+            <input type={"text"} className="form_rectangle" />
           </div>
           <div className="form_inner31">
             <label>Company</label>
-            <div className="form_rectangle"></div>
+            <br />
+            <input type={"text"} className="form_rectangle" />
           </div>
         </div>
         <div className="form_inner3">
           <div className="form_inner31">
             <label>Email</label>
-            <div className="form_rectangle"></div>
+            <br />
+            <input type={"text"} className="form_rectangle" />
           </div>
           <div className="form_inner31">
             <label>Phone</label>
-            <div className="form_rectangle"></div>
+            <br />
+            <input type={"text"} className="form_rectangle" />
           </div>
           <div className="form_inner31">
             <label>Mobile</label>
-            <div className="form_rectangle"></div>
+            <br />
+            <input type={"text"} className="form_rectangle" />
           </div>
         </div>
         <div className="form_inner3">
           <div className="form_inner31">
             <label>Addres</label>
-            <div className="form_rectangle"></div>
+            <br />
+            <input type={"text"} className="form_rectangle" />
           </div>
           <div className="form_inner31">
             <label>City</label>
-            <div className="form_rectangle"></div>
+            <br />
+            <input type={"text"} className="form_rectangle" />
           </div>
           <div className="form_inner31">
             <label>State</label>
-            <div className="form_rectangle"></div>
+            <br />
+            <input type={"text"} className="form_rectangle" />
           </div>
         </div>
         <div className="form_inner3">
           <div className="form_inner31">
             <label>Pin Code</label>
-            <div className="form_rectangle"></div>
+            <br />
+            <input type={"text"} className="form_rectangle" />
           </div>
           <div className="form_inner31">
             <label>Lead Source Name</label>
-            <div className="form_rectangle"></div>
+            <br />
+            <input type={"text"} className="form_rectangle" />
           </div>
           <div className="form_inner31">
             <label>Date Added On</label>
-            <div className="form_rectangle"></div>
+            <br />
+            <input type={"text"} className="form_rectangle" />
           </div>
         </div>
         <div className="form_inner3">
           <div className="form_inner31">
             <label>Active Status</label>
-            <div className="form_rectangle"></div>
+            <br />
+            <input type={"text"} className="form_rectangle" />
           </div>
           <div className="form_inner31">
             <label>Lead owner</label>
-            <div className="form_rectangle"></div>
+            <br />
+            <input type={"text"} className="form_rectangle" />
           </div>
           <div className="form_inner31">
             <label></label>
